0d0bbf658c4b92e128a5dfbcccc4f45fcfd94061
yysyo111 <101189476+yysyo111@users.noreply.github.com>
2022-07-30 17:52:23 +0900

    コンパイル練習

---
+++ Main.java
@@ -0,0 +1,11 @@
+import comment.Zenhan;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		comment.Zenhan.doWarusa();
+		comment.Zenhan.doTogama();
+		comment.Kouhan.callDeae();
+		comment.Kouhan.showMondokoro();
+	}
+}
+
